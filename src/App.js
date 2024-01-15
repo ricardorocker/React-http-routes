7141eb07db526e1376a29db0b303f323e161b6ec
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2024-01-15 10:07:39 -0300

    HTTP Request, css, useState, useEffect, fetch, map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState, useEffect } from "react";
+import "./style.css";
 
-
+// https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+
+    function loadApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
+          setNutri(json);
+        });
+    }
+
+    loadApi();
+  }, []);
+
   return (
-    <div>
-      <h1>TESTE</h1>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className="post">
+            <strong className="titulo">{item.titulo}</strong>
+
+            <img src={item.capa} alt={item.titulo} className="capa"></img>
+
+            <p className="subtitulo">{item.subtitulo}</p>
+
+            <a className="botao">Acessar</a>
+          </article>
+        );
+      })}
     </div>
   );
 }
